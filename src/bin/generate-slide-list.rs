28eec394a921a3cbab65cf97804b6171e8e46ebc
Olle Wreede <olle@wreede.se>
2021-05-01 23:08:55 +0200

    Add theme dropdown to slides html generator

--- src/bin/generate-slide-list.rs
+++ src/bin/generate-slide-list.rs
@@ -1,5 +1,5 @@
 use glob::glob;
-use maud::{html, Markup, DOCTYPE};
+use maud::{html, Markup, PreEscaped, DOCTYPE};
 use std::borrow::Cow;
 use std::path::PathBuf;
 use structopt::StructOpt;
@@ -12,7 +12,7 @@ use structopt::StructOpt;
 struct CliOptions {
     /// Path to directory with slides
     #[structopt(short, long, parse(from_os_str), default_value = "./assets/")]
-    pub slides: PathBuf,
+    pub path: PathBuf,
     /// Path to where html file will be saved
     #[structopt(
         short,
@@ -30,6 +30,7 @@ fn header(page_title: &str) -> Markup {
             title { (page_title) }
             meta name="viewport" content="width=device-width";
             link rel="stylesheet" href="stylesheet.css";
+            script { (PreEscaped(javascript())) }
         }
     }
 }
@@ -56,13 +57,34 @@ pub fn page(title: &str, content: Markup) -> Markup {
     }
 }
 
-struct Slide {
+struct Filename {
     path: PathBuf,
 }
 
-impl Slide {
-    fn href(&self) -> String {
-        format!("index.html?slides={}", self.path.to_str().unwrap())
+impl Filename {
+    fn href(&self, theme: Option<&Filename>) -> String {
+        format!(
+            "index.html?slides={}&theme={}",
+            self.path(),
+            match theme {
+                Some(t) => t.path(),
+                None => "",
+            }
+        )
+    }
+
+    fn files(path: &PathBuf, extension: &str) -> Vec<Self> {
+        glob(&format!("{}/*.{}", path.to_string_lossy(), extension))
+            .expect("Couldn't read files")
+            .filter(|entry| entry.is_ok())
+            .map(|entry| Filename {
+                path: entry.unwrap(),
+            })
+            .collect()
+    }
+
+    fn path(&self) -> &str {
+        self.path.to_str().unwrap()
     }
 
     fn name(&self) -> Cow<str> {
@@ -73,30 +95,41 @@ impl Slide {
     }
 }
 
+fn javascript() -> &'static str {
+    r#"
+    function change_theme(el) {
+        let url = new URL(el.href);
+        let query = new URLSearchParams(url.search);
+        const theme = document.getElementById("theme").selectedOptions[0].value;
+        query.set("theme", theme);
+        url.search = query;
+        el.href = url;
+    }
+    "#
+}
+
 fn main() {
     let opt = CliOptions::from_args();
-    let path = format!(
-        "{}/*.md",
-        opt.slides
-            .into_os_string()
-            .to_str()
-            .expect("Incorrect slides path")
-    );
-    let mut slides = vec![];
-    for entry in glob(&path).expect("Couldn't read slides") {
-        match entry {
-            Ok(path) => slides.push(Slide { path }),
-            Err(e) => println!("{:?}", e),
-        }
-    }
+
+    let slides = Filename::files(&opt.path, "md");
+    let themes = Filename::files(&opt.path, "json");
 
     let html = page(
         "Rusty Slider",
         html! {
+            form {
+                select#theme {
+                    @for theme in &themes {
+                        option value=(theme.path()) {
+                            (theme.name())
+                        }
+                    }
+                }
+            }
             ul {
                 @for slide in &slides {
                     li {
-                        a href=(slide.href()) {
+                        a href=(PreEscaped(slide.href(themes.first()))) onclick="change_theme(this)" {
                             (slide.name())
                         }
                     }
