d1d9433504ab01954b9d51a981eff8dfa48525ee
Olle Wreede <olle@wreede.se>
2022-06-03 12:47:03 +0200

    Allow compiling and executing Rust code in code blocks

--- src/executable_code.rs
+++ src/executable_code.rs
@@ -1,6 +1,8 @@
+use std::ffi::OsStr;
 use std::fmt;
 use std::io::prelude::*;
 use std::process::{Command, Stdio};
+use tempdir::TempDir;
 
 #[derive(Clone)]
 pub enum ExecutableCode {
@@ -8,6 +10,7 @@ pub enum ExecutableCode {
     Python(String),
     Ruby(String),
     Perl(String),
+    Rust(String),
 }
 
 impl fmt::Display for ExecutableCode {
@@ -17,6 +20,7 @@ impl fmt::Display for ExecutableCode {
             ExecutableCode::Python(_) => write!(f, "python"),
             ExecutableCode::Ruby(_) => write!(f, "ruby"),
             ExecutableCode::Perl(_) => write!(f, "perl"),
+            ExecutableCode::Rust(_) => write!(f, "rust"),
         }
     }
 }
@@ -28,22 +32,49 @@ impl ExecutableCode {
             "python" => Some(ExecutableCode::Python(code.to_string())),
             "ruby" => Some(ExecutableCode::Ruby(code.to_string())),
             "perl" => Some(ExecutableCode::Perl(code.to_string())),
+            "rust" => Some(ExecutableCode::Rust(code.to_string())),
             _ => None,
         }
     }
 
     pub fn execute(&self) -> String {
         match self {
-            ExecutableCode::Bash(code) => return self.execute_command("bash", "-", code),
-            ExecutableCode::Python(code) => return self.execute_command("python3", "-", code),
-            ExecutableCode::Ruby(code) => return self.execute_command("ruby", "-", code),
-            ExecutableCode::Perl(code) => return self.execute_command("perl", "-", code),
+            ExecutableCode::Bash(code) => return self.execute_command("bash", ["-"], code),
+            ExecutableCode::Python(code) => return self.execute_command("python3", ["-"], code),
+            ExecutableCode::Ruby(code) => return self.execute_command("ruby", ["-"], code),
+            ExecutableCode::Perl(code) => return self.execute_command("perl", ["-"], code),
+            ExecutableCode::Rust(code) => {
+                if let Ok(tmp_dir) = TempDir::new("rusty-slider") {
+                    if let Some(file_path) = tmp_dir.path().join("rustc.out").to_str() {
+                        self.execute_command("rustc", ["-v", "-o", file_path, "-"], code);
+                        return self.run_command_capture_output(file_path);
+                    }
+                }
+                "Error: could not create temp file".to_string()
+            }
         }
     }
 
-    fn execute_command(&self, command: &str, argument: &str, code: &String) -> String {
+    fn run_command_capture_output(&self, command: &str) -> String {
+        let process = match Command::new(command).stdout(Stdio::piped()).spawn() {
+            Err(why) => return self.error(why),
+            Ok(process) => process,
+        };
+        let mut output = String::new();
+        match process.stdout.unwrap().read_to_string(&mut output) {
+            Err(why) => return self.error(why),
+            Ok(_) => (),
+        };
+        return output;
+    }
+
+    fn execute_command<I, S>(&self, command: &str, arguments: I, code: &String) -> String
+    where
+        I: IntoIterator<Item = S>,
+        S: AsRef<OsStr>,
+    {
         let process = match Command::new(command)
-            .arg(argument)
+            .args(arguments)
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
             .spawn()
