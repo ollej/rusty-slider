bc4e33f7cce2c74ec8e4ab05f6ee7997872b5d55
Olle Wreede <olle@wreede.se>
2022-10-26 16:36:21 +0200

    Add twitter card to example page

--- src/bin/generate-slide-list.rs
+++ src/bin/generate-slide-list.rs
@@ -110,6 +110,12 @@ fn header(page_title: &str) -> Markup {
             meta charset="utf-8";
             title { (page_title) }
             meta name="viewport" content="width=device-width";
+            meta name="twitter:card" content="summary_large_image";
+            meta name="twitter:site" content="@ollej";
+            meta name="twitter:creator" content="@ollej";
+            meta name="twitter:title" content="Rusty Slider example slideshows";
+            meta name="twitter:description" content="Rusty Slider is a small tool to display markdown files as a slideshow written in Rust.";
+            meta name="twitter:image" content="https://ollej.github.io/rusty-slider/assets/screenshot.png";
             link rel="stylesheet" href="https://ollej.github.io/rusty-slider/assets/css/style.css";
             style { (PreEscaped(stylesheet())) }
             script { (PreEscaped(javascript())) }
